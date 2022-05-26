78de04c5f93fa5b1b1143d7fa77e83398e6f7719
Octavio Llanillo <llanillo4@hotmail.com>
2022-05-26 09:56:10 -0300

    Update Huffman.c

--- TP8/Huffman.c
+++ TP8/Huffman.c
@@ -7,15 +7,14 @@ typedef struct Cadidatos{
     float Frecuencia;
 } Candidato;
 
-int BuscarIndiceMenorFrecuencia(Candidato Arreglo[], int Tamano);
+int BuscarIndiceMenorFrecuencia(Candidato Arreglo[], int Tamano, int EvitarIndice);
 void VerCodigoLetras(Arbol Arbol, int contador);
 
 int main(){
 
-    /**+++++++++++++++++++++++++++++++************+++++++++++++++++++++++++++++++***************
-     +++************************++++ Ejercicio 1 - Crear Huffman +++************************++++
-     ***+++++++++++++++++++++++++++++++*********************************************************/
-    int TamanoArreglo = TAMANO;
+    #pragma region Ejercicio1
+    int IndicePrimerMenor, IndiceSegundoMenor;
+    int CantidadSubArboles = TAMANO;
     Arbol Arboles [TAMANO];
     Candidato Candidatos [] = {{'a', 0.110845}, {'b', 0.010895},
                                {'c', 0.048778}, {'d', 0.049769},
@@ -41,57 +40,55 @@ int main(){
     /*
      * Iteramos mientras el tamaño del arreglo sea mayor que 1
      */
-    while(TamanoArreglo > 1){
+    while(CantidadSubArboles > 1){
         /*
-         *  Buscamos el índice del carácter con menor frecuencia, guardamos su frecuencia en un auxiliar
-         *  y la reiniciamos
+         *  Buscamos los índices de los caracteres con menor frecuencia en el arreglo de candidatos.
          */
-        float Auxiliar;
-        int IndicePrimerMenor = BuscarIndiceMenorFrecuencia(Candidatos, TAMANO);
-        Auxiliar = Candidatos[IndicePrimerMenor].Frecuencia;
-        Candidatos[IndicePrimerMenor].Frecuencia = 2;
-
-        /*
-         * Busco el segundo carácter con menor frecuencia
-         */
-        int IndiceSegundoMenor = BuscarIndiceMenorFrecuencia(Candidatos, TAMANO);
+        IndicePrimerMenor = BuscarIndiceMenorFrecuencia(Candidatos, TAMANO, -1);
+        IndiceSegundoMenor = BuscarIndiceMenorFrecuencia(Candidatos, TAMANO, IndicePrimerMenor);
 
         /*
          * Creamos un árbol con las dos hojas de los índices encontrados y lo guardamos
-         * en el arreglo de arboles, luego aumentamos la frecuencia de la posición de mi segundo carácter
+         * en el arreglo de arboles, luego aumentamos la frecuencia del segundo carácter
+         * y asignamos a un valor alto la del primer carácter para que no se tome en cuenta.
          */
-        Arbol Final = ArmarAB(Arboles[IndicePrimerMenor], Arboles[IndiceSegundoMenor], ' ');
-        Arboles[IndiceSegundoMenor] = Final;
-        Candidatos[IndiceSegundoMenor].Frecuencia += Auxiliar;
-        TamanoArreglo--;
+        Arboles[IndiceSegundoMenor] = ArmarAB(Arboles[IndicePrimerMenor], Arboles[IndiceSegundoMenor], ' ');
+        Candidatos[IndiceSegundoMenor].Frecuencia += Candidatos[IndicePrimerMenor].Frecuencia;
+        Candidatos[IndicePrimerMenor].Frecuencia = 2;
+        CantidadSubArboles--;
     }
 
     /*
-     * Buscamos el arbol final en el arreglo
+     * Buscamos el árbol final en el arreglo
      */
-    int Indice = BuscarIndiceMenorFrecuencia(Candidatos, TAMANO);
-    Arbol Final = Arboles[Indice];
+    Arbol Final = Arboles[IndiceSegundoMenor];
     MostrarArbol(Final, 0 );
 
-    /**+++++++++++++++++++++++++++++++************+++++++++++++++++++++++++++++++***************
-     ***********************++++ Ejercicio 2 - Código de cada letra ***++++++++*****************
-     ***+++++++++++++++++++++++++++++++*********************************************************/
+    #pragma endregion
+
+    #pragma region Ejercicio2
+
     VerCodigoLetras(Final, 0);
 
+    #pragma endregion
 
+    #pragma region Ejercicio3
+
+    #pragma endregion
 
     return 0;
 }
 
 /*
- * Busca el índice del carácter con menor frecuencia en el arreglo de candidatos
+ * Busca el índice del carácter con menor frecuencia en el arreglo de candidatos.
+ * El últimmo parámetro indica que índice ignorar
  */
-int BuscarIndiceMenorFrecuencia(Candidato Arreglo[], int Tamano){
+int BuscarIndiceMenorFrecuencia(Candidato Arreglo[], int Tamano, int EvitarIndice){
     int Indice = 0;
-    float Menor = 20.0f;
+    float Menor = 20;
 
     for(unsigned int i = 0; i < Tamano; i++){
-        if(Arreglo[i].Frecuencia < Menor){
+        if(i != EvitarIndice && Arreglo[i].Frecuencia < Menor){
             Indice = (int) i;
             Menor = Arreglo[i].Frecuencia;
         }
